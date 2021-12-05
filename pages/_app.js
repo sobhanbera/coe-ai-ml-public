b6f19f42aa88f9a49acdf35fdf02af2ea92ff102
sobhanbera <sobhanbera258@gmail.com>
2021-12-05 20:54:26 +0530

    constants

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <div>
+            <Header />
+            <div className="main-content">
+                <Component {...pageProps} />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default MyApp
