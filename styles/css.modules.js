7b893964b4e3571ae09b0eab59dc2f7d7ea60ce8
sobhanbera <sobhanbera258@gmail.com>
2021-12-05 20:48:57 +0530

    added styles for different components

---
+++ styles/css.modules.js
@@ -0,0 +1,13 @@
+import header from './components/header.module.scss'
+
+const componentStyles = {
+    headerStyles: header,
+}
+
+import home from './pages/home.module.css'
+
+const pageStyles = {
+    homeStyles: home,
+}
+
+export {componentStyles, pageStyles}
