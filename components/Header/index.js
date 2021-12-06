6ab322c05d362f8e04946740775881ee13d8c7c4
sobhanbera <sobhanbera258@gmail.com>
2021-12-07 00:17:06 +0530

    header component compledted for small screens too

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
+import {useRouter} from 'next/router'
 
 import {componentStyles} from '../../styles/css.modules'
 
 export default function Header() {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const pathname = useRouter().pathname
+
     return (
         <header className={componentStyles.headerStyles.header}>
             <Head>
@@ -14,15 +19,17 @@ export default function Header() {
 
             <div className={componentStyles.headerStyles.content}>
                 <div className={componentStyles.headerStyles.logo}>
-                    <a>
-                        <Image
-                            src="/img/ghrce.png"
-                            alt="ghrce logo"
-                            width={40}
-                            height={40}
-                        />
-                        &nbsp;COE-AIML
-                    </a>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/img/ghrce.png"
+                                alt="ghrce logo"
+                                width={40}
+                                height={40}
+                            />
+                            &nbsp;COE-AIML
+                        </a>
+                    </Link>
                 </div>
 
                 <nav>
@@ -59,6 +66,94 @@ export default function Header() {
                         </li>
                     </ul>
                 </nav>
+
+                {/* Phone Part */}
+                <div
+                    className={
+                        componentStyles.headerStyles.smallScreenComponent
+                    }>
+                    <Image
+                        onClick={() => setShowMenu(value => !value)}
+                        src="/img/menu.png"
+                        width={22}
+                        height={22}
+                        alt="menu icon"
+                        className={componentStyles.headerStyles.hamburgerIcon}
+                    />
+
+                    {showMenu ? (
+                        <div className={componentStyles.headerStyles.tabsList}>
+                            <ul>
+                                <li
+                                    className={
+                                        pathname === '/'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/">
+                                        <a>Home</a>
+                                    </Link>
+                                </li>
+                                <li
+                                    className={
+                                        pathname === '/projects'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/projects">
+                                        <a>Projects</a>
+                                    </Link>
+                                </li>
+                                <li
+                                    className={
+                                        pathname === '/peoples'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/peoples">
+                                        <a>Peoples</a>
+                                    </Link>
+                                </li>
+                                <li
+                                    className={
+                                        pathname === '/thesis'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/thesis">
+                                        <a>Thesis</a>
+                                    </Link>
+                                </li>
+                                <li
+                                    className={
+                                        pathname === '/publications'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/publications">
+                                        <a>Publications</a>
+                                    </Link>
+                                </li>
+                                <li
+                                    className={
+                                        pathname === '/contactus'
+                                            ? componentStyles.headerStyles
+                                                  .listItemActive
+                                            : null
+                                    }>
+                                    <Link href="/contactus">
+                                        <a>Contact</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    ) : null}
+                </div>
             </div>
         </header>
     )
