c9218970c3c475bb382d875535529bb76e688420
sobhanbera <sobhanbera258@gmail.com>
2021-12-07 13:58:44 +0530

    new header image added in main home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
+import Image from 'next/image'
+import {useRouter} from 'next/router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({Component, pageProps}) {
+    const pathname = useRouter().pathname
+
     return (
         <div>
             <Header />
+
+            {pathname === '/' && (
+                <div className={'header-image'}>
+                    <div className="header-content">
+                        <img src="/img/ghrce.png" alt="ghrce logo" />
+                    </div>
+                </div>
+            )}
+
             <div className="main-content">
                 <Component {...pageProps} />
             </div>
