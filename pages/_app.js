bc44169ffc7c788d0489a1c36d7fb4c92fb4c3f9
sobhanbera <sobhanbera258@gmail.com>
2021-12-08 17:11:09 +0530

    global components updated with shifting of components to the respective pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,13 @@
-import Image from 'next/image'
-import {useRouter} from 'next/router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({Component, pageProps}) {
-    const pathname = useRouter().pathname
-
     return (
         <div>
             <Header />
 
-            {pathname === '/' && (
-                <div className={'header-image'}>
-                    <div className="header-content">
-                        <img src="/img/ghrce.png" alt="ghrce logo" />
-                    </div>
-                </div>
-            )}
-
-            <div className="main-content">
+            <div className="main-content-global">
                 <Component {...pageProps} />
             </div>
             <Footer />
