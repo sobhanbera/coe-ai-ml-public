a095ad9dea7d06cfe4c5430c709e8776623c673f
sobhanbera <sobhanbera258@gmail.com>
2021-12-31 18:39:12 +0000

    backing up linux to build a new kernel just after this

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import {pageStyles} from '../styles/css.modules'
+import MainSection from '../components/MainSection'
+
+export default function Projects() {
+    return (
+        <div className={pageStyles.homeStyles.container}>
+            <MainSection> Main Content</MainSection>
+        </div>
+    )
+}
---
+++ pages/publications.js
@@ -0,0 +1,10 @@
+import {pageStyles} from '../styles/css.modules'
+import MainSection from '../components/MainSection'
+
+export default function Publications() {
+    return (
+        <div className={pageStyles.homeStyles.container}>
+            <MainSection> Main Content</MainSection>
+        </div>
+    )
+}
